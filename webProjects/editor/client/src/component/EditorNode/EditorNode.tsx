c93eba62b882a7fc55e9bfc96980efe528d33a2b
Creativetools90 <coderx523@gmail.com>
2024-06-28 21:17:48 +0530

    resizer

--- webProjects/editor/client/src/component/EditorNode/EditorNode.tsx
+++ webProjects/editor/client/src/component/EditorNode/EditorNode.tsx
@@ -6,22 +6,42 @@ import EditPad from './EditPad';
 import Output from './output/Output';
 
 const EditorNode = () => {
-    const { input, setInput, innerHTML, setInputHtml } = useContext(ThemeContext)
+    const { input, setInput, innerHTML, setInputHtml } = useContext(ThemeContext);
 
-    const [quitwindow , setQuitWindow] = useState();
+    const [quitwindow, setQuitWindow] = useState(false);
+    const [fullwindow, setFullWindow] = useState(false);
 
     return (
         <div className="EditorNode leftView">
             <div className="editPad left_editor_pad">
                 <EditPad val={setInput} htmlVal={setInputHtml} />
             </div>
-            <div className="result left_result_pad" style={quitwindow?{flex:1}:{flex:2}} >
-               <div className="tabO">
-               <div className="outputTab">
-                    <img width="20" height="20" src="https://img.icons8.com/ios/50/000000/full-screen--v1.png" alt="full-screen--v1" />
-                    <img onClick={()=>setQuitWindow(!quitwindow)} width="20" height="20" src="https://img.icons8.com/ios/50/export.png" alt="export"/>
+            <div 
+                className={`result left_result_pad`} 
+                style={{ 
+                    flex: quitwindow ? 0.2 : 2, 
+                }
+                
+            }
+            >
+                <div className="tabO">
+                    <div className="outputTab">
+                        {/* <img 
+                            onClick={() => setFullWindow(!fullwindow)} 
+                            width="20" 
+                            height="20" 
+                            src="https://img.icons8.com/ios/50/000000/full-screen--v1.png" 
+                            alt="full-screen--v1" 
+                        /> */}
+                        <img 
+                            onClick={() => setQuitWindow(!quitwindow)} 
+                            width="20" 
+                            height="20" 
+                            src="https://img.icons8.com/ios/50/export.png" 
+                            alt="export" 
+                        />
+                    </div>
                 </div>
-               </div>
                 <Output langInput={input} />
             </div>
         </div>
