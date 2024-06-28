db8257280eebb33ec8a90dea45190087cbe1d9c1
Creativetools90 <coderx523@gmail.com>
2024-06-29 04:20:12 +0530

    banner

--- webProjects/editor/client/src/webComponent/homePage/Home.tsx
+++ webProjects/editor/client/src/webComponent/homePage/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Home.css"
 const Home = () => {
     return (
-        <div>
-            Home
-            <Link to="/demoCode" >try demo code</Link>
+        <div className='homePageContainer p-2 m-auto my-4 ' >
+            <div className="BannerHeroContainer gap-2  flex flex-col  justify-center ">
+                <div className="bannerHeading  p-2 text-center ">
+                    <h2>Try & Practice your code in webEditor</h2>
+                </div>
+                <div className="tryNow flex p-2 items-center gap-6 flex-row justify-center ">
+
+                    <Link to="/demoCode" >try demo code</Link>
+                    <Link to="/demoCode" >Make Project</Link>
+                </div>
+            </div>
+            <div className="other"></div>
         </div>
     )
 }
