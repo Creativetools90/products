df917721e766f81809b9ce624d1aaa31d852874b
Creativetools90 <coderx523@gmail.com>
2024-06-28 21:05:06 +0530

    doneleft_result_pad

--- webProjects/editor/client/src/component/EditorNode/EditorNode.tsx
+++ webProjects/editor/client/src/component/EditorNode/EditorNode.tsx
@@ -6,18 +6,23 @@ import EditPad from './EditPad';
 import Output from './output/Output';
 
 const EditorNode = () => {
-    const {input , setInput , innerHTML , setInputHtml} = useContext(ThemeContext)
+    const { input, setInput, innerHTML, setInputHtml } = useContext(ThemeContext)
 
-
+    const [quitwindow , setQuitWindow] = useState();
 
     return (
         <div className="EditorNode leftView">
             <div className="editPad left_editor_pad">
-               <EditPad val={setInput} htmlVal={setInputHtml} />
+                <EditPad val={setInput} htmlVal={setInputHtml} />
             </div>
-            <div className="result left_result_pad">
-               
-              <Output langInput={input} />
+            <div className="result left_result_pad" style={quitwindow?{flex:1}:{flex:2}} >
+               <div className="tabO">
+               <div className="outputTab">
+                    <img width="20" height="20" src="https://img.icons8.com/ios/50/000000/full-screen--v1.png" alt="full-screen--v1" />
+                    <img onClick={()=>setQuitWindow(!quitwindow)} width="20" height="20" src="https://img.icons8.com/ios/50/export.png" alt="export"/>
+                </div>
+               </div>
+                <Output langInput={input} />
             </div>
         </div>
     );
