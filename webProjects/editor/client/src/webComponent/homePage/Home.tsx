6995d056e5b8e69636cfad6d76be78e0ada904da
Creativetools90 <coderx523@gmail.com>
2024-06-29 06:22:49 +0530

    democode

--- webProjects/editor/client/src/webComponent/homePage/Home.tsx
+++ webProjects/editor/client/src/webComponent/homePage/Home.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Editor from "@monaco-editor/react";
 import "./Home.css"
 const Home = () => {
+    const [demoInput , setDemoInput] = useState(`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<style>
+    button{
+        background : #4158D0;
+        color:white;
+        padding:10px;
+        }
+</style>
+</head>
+<body>
+<h1>Creativetools WebCode_editor</h1>
+<p>this si demo code contaienr </p>
+<button>do coding </button>
+
+<!-- Prss Enter -->
+</body>
+</html>
+`);
     return (
         <div className='homePageContainer p-2 m-auto my-4 ' >
             <div className="BannerHeroContainer  ">
@@ -23,9 +46,7 @@ const Home = () => {
                 <h3 className='c_heading' >Languages CodeBase</h3>
                 <div className="code_snippet">
                     <div className="s_01_">
-                        <div className="codeVisibleLayer">
-                            <p>click for preview</p>
-                        </div>
+
                         <Editor
                             height="calc(100vh - 50px)"
                             width="100%"
@@ -61,9 +82,7 @@ const Home = () => {
                         />
                     </div>
                     <div className="s_01_">
-                    <div className="codeVisibleLayer">
-                            <p>click for preview</p>
-                        </div>
+
                         <Editor
                             height="calc(100vh - 50px)"
                             width="100%"
@@ -95,9 +114,7 @@ counter();
                         />
                     </div>
                     <div className="s_01_">
-                    <div className="codeVisibleLayer">
-                            <p>click for preview</p>
-                        </div>
+
                         <Editor
                             height="calc(100vh - 50px)"
                             width="100%"
@@ -127,7 +144,83 @@ counter()
                             }}
                         />
                     </div>
-                   
+
+                </div>
+            </section>
+            <section className="Movingcontainer">
+                <div className="wrap">
+                    <div className="scroller">
+                        <p className="item">html</p>
+                        <p className="item">css</p>
+                        <p className="item">javaScript</p>
+                        <p className="item">typescript</p>
+                        <p className="item">c++</p>
+                        <p className="item">c</p>
+                        <p className="item">java</p>
+                    </div>
+
+
+
+                </div>
+            </section>
+            <section className='demoHomeCode' >
+                <div className="cvw-01-heaing">
+                   <p> how doing some demo editing !</p>
+                </div>
+                <div className="democodeBaseLayout">
+                    <div className="s_01_01">
+
+                        <Editor
+                            height="calc(100vh - 50px)"
+                            width="100%"
+                            theme={"vs-dark"} // Change this to 'vs', 'vs-dark', or 'hc-black'
+                            onChange={(e:any)=>setDemoInput(e)}
+                            defaultLanguage="html"
+                            defaultValue={`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<style>
+    button{
+        background : #4158D0;
+        color:white;
+        padding:10px;
+        }
+</style>
+</head>
+<body>
+<h1>Creativetools WebCode_editor</h1>
+<p>this si demo code contaienr </p>
+<button>do coding </button>
+<!-- Prss Enter -->
+</body>
+</html>
+`}
+                            options={{
+                                fontSize: 15,
+                                minimap: { enabled: false },
+                                lineNumbers: "on",
+                                automaticLayout: true,
+                                padding: {
+                                    top: 10,
+                                    bottom: 10
+                                },
+
+                                wordWrap: "on", // Enable word wrap
+                                formatOnType: true, // Disable format on type
+                                formatOnPaste: true, // Disable format on paste
+                            }}
+                        />
+                    </div>
+                    <div className="demores">
+                    <iframe
+                title="Output"
+                style={{ width: '100%', height: '100%', border: 'none' }}
+                srcDoc={demoInput}
+            />
+                    </div>
                 </div>
             </section>
         </div>
